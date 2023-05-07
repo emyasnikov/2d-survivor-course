901a9fb0160d596141cc87b11e418ab422068aa7
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-07 20:00:55 +0200

    Added sword instance.

--- scenes/abilities/sword_ability/sword_ability_controller.gd
+++ scenes/abilities/sword_ability/sword_ability_controller.gd
@@ -13,4 +13,13 @@ func _process(delta):
 
 
 func _on_timer_timeout():
-    print("ping")
+    var player = get_tree().get_first_node_in_group("player") as Node2D
+
+    if player == null:
+        return
+
+    var sword_instance = sword_ability.instantiate() as Node2D
+
+    player.get_parent().add_child(sword_instance)
+    sword_instance.global_position = player.global_position
+
