ed87f9f6ed5f3caa541ac3081006654902d22c44
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-07 18:01:54 +0200

    Refactored sword ability controller.

--- scenes/abilities/sword_ability/sword_ability_controller.gd
+++ scenes/abilities/sword_ability/sword_ability_controller.gd
@@ -4,7 +4,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $Timer.timeout(on_timer_timeout)
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,5 +12,5 @@ func _process(delta):
     pass # Replace with function body.
 
 
-func on_timer_timeout():
+func _on_timer_timeout():
     print("ping")
