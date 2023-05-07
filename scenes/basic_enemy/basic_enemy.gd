d6dd02f260ed28b937779ab4ccba4379e6b366b2
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-07 23:09:31 +0200

    Added damage to sword.

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -23,3 +23,7 @@ func get_direction_to_player():
         return (player_node.global_position - global_position).normalized()
 
     return Vector2.ZERO
+
+
+func _on_area_2d_area_entered(area):
+    queue_free()
