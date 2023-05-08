5946b592c2ba5c3ac7900b000c28c4cac13a8d21
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-08 18:20:11 +0200

    Refactored sword ability controller.

--- scenes/abilities/sword_ability/sword_ability_controller.gd
+++ scenes/abilities/sword_ability/sword_ability_controller.gd
@@ -9,11 +9,6 @@ func _ready():
     pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass # Replace with function body.
-
-
 func _on_timer_timeout():
     var player = get_tree().get_first_node_in_group("player") as Node2D
 
