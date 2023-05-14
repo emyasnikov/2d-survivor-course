173a3cabbbd6679d21a0869e367d049242110060
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-14 10:27:30 +0200

    Added experience manager.

---
+++ scenes/managers/experience_manager/experience_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var current_experience = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
+
+
+func increment_experience(number: float):
+    current_experience += number
+
+
+func on_experience_vial_collected(number: float):
+    increment_experience(number)
