155db49eb6179544617ec9e11f64b10fe17d74dd
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-14 22:46:01 +0200

    Fixed issue with missing parameter.

--- scenes/autoload/game_events/game_events.gd
+++ scenes/autoload/game_events/game_events.gd
@@ -4,4 +4,4 @@ signal experience_vial_collected(number: float)
 
 
 func emit_experience_vial_collected(number: float):
-    experience_vial_collected.emit()
+    experience_vial_collected.emit(number)
