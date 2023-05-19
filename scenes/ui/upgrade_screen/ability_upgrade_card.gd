cbd0eccb964a4e26f4b0fae1ec855f3c55452aa3
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-19 21:48:54 +0200

    Added selected event to ability upgrade card.

--- scenes/ui/upgrade_screen/ability_upgrade_card.gd
+++ scenes/ui/upgrade_screen/ability_upgrade_card.gd
@@ -1,8 +1,18 @@
 extends PanelContainer
 class_name AbilityUpgradeCard
 
+signal selected
+
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
+
+func _ready():
+    gui_input.connect(on_gui_input)
+
+
+func on_gui_input(event: InputEvent):
+    if event.is_action_pressed("left_click"):
+        selected.emit()
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
