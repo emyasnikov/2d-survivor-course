2adeb1a4a44d46f639ef8f7918684a4895ea6d8c
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-19 21:49:14 +0200

    Added selected event to upgrade screen.

--- scenes/ui/upgrade_screen/upgrade_screen.gd
+++ scenes/ui/upgrade_screen/upgrade_screen.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 class_name UpgradeScreen
 
+signal upgrade_selected(upgrade: AbilityUpgrade)
+
 @export var upgrade_card_scene: PackedScene
 @onready var card_container: HBoxContainer = %CardContainer
 
@@ -9,9 +11,16 @@ func _ready():
     get_tree().paused = true
 
 
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+    upgrade_selected.emit(upgrade)
+    get_tree().paused = false
+    queue_free()
+
+
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
     for upgrade in upgrades:
         var card_instance = upgrade_card_scene.instantiate() as AbilityUpgradeCard
 
         card_container.add_child(card_instance)
         card_instance.set_ability_upgrade(upgrade)
+        card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
