5b38c04623facfdee462f5e837eefe8cf35e2985
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-19 22:12:42 +0200

    Added ability upgrade added to sword ability controller.

--- scenes/abilities/sword_ability/sword_ability_controller.gd
+++ scenes/abilities/sword_ability/sword_ability_controller.gd
@@ -4,11 +4,24 @@ const MAX_RANGE = 150
 
 @export var sword_ability: PackedScene
 
+var base_wait_time
 var damage = 5
 
 
 func _ready():
+    base_wait_time = $Timer.wait_time
     $Timer.timeout.connect(on_timer_timeout)
+    GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, upgrades: Dictionary):
+    if upgrade.id != "sword_rate":
+        return
+
+    var percent_reduction = upgrades["sword_rate"]["quantity"] * 0.1
+
+    $Timer.wait_time = base_wait_time * (1 - percent_reduction)
+    $Timer.start()
 
 
 func on_timer_timeout():
