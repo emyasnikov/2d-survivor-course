8370deba9dd150c791568a2e652d7d1790d3880e
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-20 22:08:39 +0200

    Added health to player.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,16 @@ extends CharacterBody2D
 const ACCELERATION_SMOOTHING = 25
 const MAX_SPEED = 150
 
+@onready var damage_interval_timer = $DamageIntervalTimer
+
+var number_colliding_bodies = 0
+
+
+func _ready():
+    $CollisionArea.body_entered.connect(on_body_entered)
+    $CollisionArea.body_exited.connect(on_body_exited)
+    damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+
 
 func _process(delta):
     var movement_vector = get_movement_vector()
@@ -13,8 +23,29 @@ func _process(delta):
     move_and_slide()
 
 
+func check_deal_damage():
+    if number_colliding_bodies == 0 or not damage_interval_timer.is_stopped():
+        return
+
+    $HealthComponent.damage(1)
+    damage_interval_timer.start()
+
+
 func get_movement_vector():
     var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
     var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
     return Vector2(x_movement, y_movement)
+
+
+func on_body_entered(body: Node2D):
+    number_colliding_bodies += 1
+    check_deal_damage()
+
+
+func on_body_exited(body: Node2D):
+    number_colliding_bodies = max(number_colliding_bodies - 1, 0)
+
+
+func on_damage_interval_timer_timeout():
+    check_deal_damage()
