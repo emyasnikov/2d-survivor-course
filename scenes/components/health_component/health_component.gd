fb2d00c18f9cc8de317c6323b9ad8de2c1839d9a
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-22 22:14:20 +0200

    Added signal and getter to health component.

--- scenes/components/health_component/health_component.gd
+++ scenes/components/health_component/health_component.gd
@@ -2,6 +2,7 @@ extends Node
 class_name HealthComponent
 
 signal died
+signal health_changed
 
 @export var max_health: float = 10
 
@@ -14,6 +15,7 @@ func _ready():
 
 func damage(damage_amount: float):
     current_health = max(current_health - damage_amount, 0)
+    health_changed.emit()
     Callable(check_death).call_deferred()
 
 
@@ -21,3 +23,10 @@ func check_death():
     if current_health == 0:
         died.emit()
         owner.queue_free()
+
+
+func get_health_percent():
+    if max_health <= 0:
+        return
+
+    return min(current_health / max_health, 1)
