34f126ce8d3e707173da6ea8aec26b94f7c0dd8f
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-22 23:50:17 +0200

    Added defeat to end screen.

--- scenes/ui/end_screen/end_screen.gd
+++ scenes/ui/end_screen/end_screen.gd
@@ -16,3 +16,8 @@ func on_restart_button_pressed():
     get_tree().paused = false
 
     get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func set_defeat():
+    %TitleLabel.text = "Defeat"
+    %DescriptionLabel.text = "You lost!"
