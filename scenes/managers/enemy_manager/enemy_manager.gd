e60c5d905f657ad137660317f3ee2b81d64af0c5
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-23 18:02:11 +0200

    Changed spawn rate in enemy manager.

--- scenes/managers/enemy_manager/enemy_manager.gd
+++ scenes/managers/enemy_manager/enemy_manager.gd
@@ -2,20 +2,30 @@ extends Node
 
 const SPAWN_RADIUS = 340
 
+@onready var timer = $Timer
+
 @export var arena_time_manager: ArenaTimeManager
 @export var basic_enemy_scene: PackedScene
+
+var base_spawn_time = 0
 
 
 func _ready():
     arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
-    $Timer.timeout.connect(on_timer_timeout)
+    timer.timeout.connect(on_timer_timeout)
+
+    base_spawn_time = timer.wait_time
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
-    pass
+    var time_off = max((0.1 / 12) * arena_difficulty, 0.7)
+
+    timer.wait_time = base_spawn_time - time_off
 
 
 func on_timer_timeout():
+    timer.start()
+
     var player = get_tree().get_first_node_in_group("player") as Node2D
 
     if player == null:
