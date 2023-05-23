899293a93ce406cd88a5c2e69e753ed2d5d90c9c
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-05-23 19:57:16 +0200

    Added spawn position getter to enemy manager.

--- scenes/managers/enemy_manager/enemy_manager.gd
+++ scenes/managers/enemy_manager/enemy_manager.gd
@@ -17,6 +17,16 @@ func _ready():
     base_spawn_time = timer.wait_time
 
 
+func get_spawn_position(player):
+    if player == null:
+        return Vector2.ZERO
+
+    var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+    var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
+    return spawn_position
+
+
 func on_arena_difficulty_increased(arena_difficulty: int):
     var time_off = max((0.1 / 12) * arena_difficulty, 0.7)
 
@@ -31,10 +41,8 @@ func on_timer_timeout():
     if player == null:
         return
 
-    var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-    var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
     var enemy = basic_enemy_scene.instantiate() as Node2D
     var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 
     entities_layer.add_child(enemy)
-    enemy.global_position = spawn_position
+    enemy.global_position = get_spawn_position(player)
