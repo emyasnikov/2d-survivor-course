281b97836a31bb35c2da32d551611c75b3431217
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-07-09 11:06:47 +0200

    Refactored axe ability.

--- scenes/abilities/axe_ability/axe_ability.gd
+++ scenes/abilities/axe_ability/axe_ability.gd
@@ -13,16 +13,13 @@ func _ready():
 func tween_method(rotations: float):
     var current_direction = Vector2.RIGHT.rotated(rotations * TAU)
     var percent = rotations / 2
-    var root_position = Vector2.ZERO
 
     var current_radius = percent * MAX_RADIUS
 
     var player = get_tree().get_first_node_in_group("player")
 
     if player == null:
-        root_position = global_position
-    else:
-        root_position = player.global_position
+        return
 
-    global_position = root_position + (current_direction * current_radius)
+    global_position = player.global_position + (current_direction * current_radius)
 
