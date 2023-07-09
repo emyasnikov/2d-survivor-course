298189f8735016d4a0f00f2995f02f8ab64dc108
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-07-09 11:05:18 +0200

    Added tween method to axe ability.

--- scenes/abilities/axe_ability/axe_ability.gd
+++ scenes/abilities/axe_ability/axe_ability.gd
@@ -1,2 +1,28 @@
-extends Node
+extends Node2D
 class_name AxeAbility
+
+const MAX_RADIUS = 100
+
+
+func _ready():
+    var tween = create_tween()
+
+    tween.tween_method(tween_method, 0, 2, 2)
+
+
+func tween_method(rotations: float):
+    var current_direction = Vector2.RIGHT.rotated(rotations * TAU)
+    var percent = rotations / 2
+    var root_position = Vector2.ZERO
+
+    var current_radius = percent * MAX_RADIUS
+
+    var player = get_tree().get_first_node_in_group("player")
+
+    if player == null:
+        root_position = global_position
+    else:
+        root_position = player.global_position
+
+    global_position = root_position + (current_direction * current_radius)
+
