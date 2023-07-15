7dc2ae7456952aed152cf901bc1c9c9752273456
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-07-15 22:33:03 +0200

    Refactored upgrade manager.

--- scenes/managers/upgrade_manager/upgrade_manager.gd
+++ scenes/managers/upgrade_manager/upgrade_manager.gd
@@ -11,19 +11,6 @@ func _ready():
     experience_manager.level_up.connect(on_level_up)
 
 
-func on_level_up(new_level: int):
-    var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-
-    if chosen_upgrade == null:
-        return
-
-    var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
-
-    add_child(upgrade_screen_instance)
-    upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-    upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
-
-
 func apply_upgrade(upgrade: AbilityUpgrade):
     var has_upgrade = current_upgrades.has(upgrade.id)
 
@@ -36,6 +23,19 @@ func apply_upgrade(upgrade: AbilityUpgrade):
         current_upgrades[upgrade.id]["quantity"] += 1
 
     GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
+
+
+func on_level_up(new_level: int):
+    var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
+
+    if chosen_upgrade == null:
+        return
+
+    var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
+
+    add_child(upgrade_screen_instance)
+    upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+    upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
